a0f67f914633b44f282c6925a4a1256ffd51af56
Mike Adeleke <michael.o@cyntonisca.com>
2025-12-10 14:01:11 +0100

    fix signup api

--- smart-law-office/src/app/api/api.ts
+++ smart-law-office/src/app/api/api.ts
@@ -48,49 +48,49 @@ api.interceptors.response.use(
 );
 // Upload API FOr FIle upload and Firm
 // seperate instance for FirmProfile Store
-export const CompleteSignupApi = axios.create({
-  baseURL,
-  headers: {
-    "Content-Type": "multipart/form-data",
-    Accept: "*/*"
-  },
-  withCredentials: false
-});
+// export const CompleteSignupApi = axios.create({
+//   baseURL,
+//   headers: {
+//     "Content-Type": "multipart/form-data",
+//     Accept: "*/*"
+//   },
+//   withCredentials: false
+// });
 
 // Request Interceptor for upload API
-CompleteSignupApi.interceptors.request.use(
-  (config) => {
-    //
-    console.log("Upload Request", {
-      method: config.method?.toUpperCase(),
-      url: `${config.baseURL}${config.url}`,
-      dataType:
-        config.data instanceof FormData ? "FormData" : typeof config.data
-    });
-    return config;
-  },
-  (error) => {
-    console.error("Upload Request Error:", error);
-    return Promise.reject(error);
-  }
-);
+// CompleteSignupApi.interceptors.request.use(
+//   (config) => {
+//     //
+//     console.log("Upload Request", {
+//       method: config.method?.toUpperCase(),
+//       url: `${config.baseURL}${config.url}`,
+//       dataType:
+//         config.data instanceof FormData ? "FormData" : typeof config.data
+//     });
+//     return config;
+//   },
+//   (error) => {
+//     console.error("Upload Request Error:", error);
+//     return Promise.reject(error);
+//   }
+// );
 
-// Response Interceptor for upload API
-CompleteSignupApi.interceptors.response.use(
-  (response) => {
-    console.log("Upload Response:", response.status, response.data);
-    return response;
-  },
-  (error) => {
-    console.error("Upload Response Error:", {
-      message: error.message,
-      code: error.code,
-      response: error.response?.data,
-      status: error.response?.status,
-      url: error.config?.url
-    });
-    return Promise.reject(error);
-  }
-);
+// // Response Interceptor for upload API
+// CompleteSignupApi.interceptors.response.use(
+//   (response) => {
+//     console.log("Upload Response:", response.status, response.data);
+//     return response;
+//   },
+//   (error) => {
+//     console.error("Upload Response Error:", {
+//       message: error.message,
+//       code: error.code,
+//       response: error.response?.data,
+//       status: error.response?.status,
+//       url: error.config?.url
+//     });
+//     return Promise.reject(error);
+//   }
+// );
 
 export default api;
